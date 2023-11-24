9dea2dbfbda849cc0932c00ca55c257a65ceee8e
AboodXD <aboood40091@gmail.com>
2023-11-24 23:45:49 +0400

    Enable resizing window

--- src/main.cpp
+++ src/main.cpp
@@ -2,11 +2,19 @@
 
 #include <rio.h>
 
+static const rio::InitializeArg cInitializeArg = {
+    .window = {
+#if RIO_IS_WIN
+        .resizable = true
+#endif // RIO_IS_WIN
+    }
+};
+
 int main()
 {
     // Initialize RIO with root task
-    if (!rio::Initialize<MainWindow>())
-        return false;
+    if (!rio::Initialize<MainWindow>(cInitializeArg))
+        return -1;
 
     // Main loop
     rio::EnterMainLoop();
