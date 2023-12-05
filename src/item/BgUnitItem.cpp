df9501762783c950250991adab05647d84884dba
AboodXD <aboood40091@gmail.com>
2023-12-05 13:31:48 +0400

    item/BgUnitItem: drawSelectionUI(): Better bound checking, update Bg{Renderer}

--- src/item/BgUnitItem.cpp
+++ src/item/BgUnitItem.cpp
@@ -2,6 +2,7 @@
 #include <MainWindow.h>
 #include <course/Bg.h>
 #include <course/BgRenderer.h>
+#include <course/BgUnitFile.h>
 #include <item/BgUnitItem.h>
 
 #include <rio.h>
@@ -41,17 +42,37 @@ void BgUnitItem::move(s16 dx, s16 dy, bool commit)
 
 void BgUnitItem::drawSelectionUI()
 {
+    const CourseDataFile* p_cd_file = static_cast<MainWindow*>(rio::sRootTask)->getCourseView()->getCourseDataFile();
+    RIO_ASSERT(p_cd_file != nullptr);
+
     u16 env = (mBgCourseData.type >> 12) & 3;
     u16 idx = mBgCourseData.type & 0xFFF;
 
     ImGui::Text("Object");
     ImGui::Separator();
 
-    const u16 min_value = 0, tileset_max = 3, index_max = 0xFFF;
-    ImGui::DragScalar("Tileset", ImGuiDataType_U16, &env, 1.0f, &min_value, &tileset_max, nullptr, ImGuiSliderFlags_AlwaysClamp);
-    ImGui::DragScalar("Index", ImGuiDataType_U16, &idx, 1.0f, &min_value, &index_max, nullptr, ImGuiSliderFlags_AlwaysClamp);
-    ImGui::DragScalarN("Size", ImGuiDataType_U16, &mBgCourseData.size, 2);
-    ImGui::DragScalar("Flag", ImGuiDataType_U8, &mBgCourseData.flag);
+    const u16 min_value_u16 = 0, env_max = CD_FILE_ENV_MAX_NUM - 1, index_max = 0xFFF;
+    const u8 min_value_u8 = 0, flag_max = 24;
 
-    mBgCourseData.type = (env << 12) | idx;
+    bool type_modified = ImGui::DragScalar("Environment Slot", ImGuiDataType_U16, &env, 1.0f, &min_value_u16, &env_max, nullptr, ImGuiSliderFlags_AlwaysClamp);
+    type_modified     |= ImGui::DragScalar("Object Index", ImGuiDataType_U16, &idx, 1.0f, &min_value_u16, &index_max, nullptr, ImGuiSliderFlags_AlwaysClamp);
+    bool size_modified = ImGui::DragScalarN("Size", ImGuiDataType_U16, &mBgCourseData.size, 2);
+    bool flag_modified = ImGui::DragScalar("Flag", ImGuiDataType_U8, &mBgCourseData.flag, 1.0f, &min_value_u8, &flag_max, nullptr, ImGuiSliderFlags_AlwaysClamp);
+
+    if (type_modified)
+    {
+        const BgUnitFile* file = Bg::instance()->getUnitFile(p_cd_file->getEnvironment(env));
+        if (file == nullptr || idx >= file->getBgUnitCount())
+            type_modified = false;
+    }
+
+    if (type_modified)
+        mBgCourseData.type = (env << 12) | idx;
+
+    if (type_modified | size_modified | flag_modified)
+    {
+        Bg::instance()->processBgCourseData(*p_cd_file);
+        BgRenderer::instance()->createVertexBuffer(mItemID.getIndex() >> 22);
+        BgRenderer::instance()->calcSelectionVertexBuffer({ mItemID });
+    }
 }
